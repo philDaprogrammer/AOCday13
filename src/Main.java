dcbf59f313d9ead10a7df03d7345ff95219b24c7
philDaprogrammer <75814801+philDaprogrammer@users.noreply.github.com>
2022-01-06 19:31:39 -0500

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String args[]) {
-        parser p = new parser("input.txt");
-        char[][] paper = p.getPaper();
-
-        solution sol = new solution(paper, new int[]{7}, new int[]{5});
-        sol.foldPaper();
-    }
-}
